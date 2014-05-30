33d9079999eb687e6adc943dd9a85e76c119e324
Andrew Koines <akoines@zoho.com>
2014-05-30 10:45:58 -0700

    Added rectangle solution

--- week_8_and_9/3_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/3_Ruby/rectangle_class/my_solution.rb
@@ -20,7 +20,25 @@ class Rectangle
     (other.width  == self.width && other.height == self.height ) ||
     (other.height == self.width && other.width  == self.height )
   end
+  
+  def area
+    @width*@height
+  end
+  
+  def perimeter
+      (2*@width) + (2*@height) 
+  end
+  
+  def diagonal
+       (Math.sqrt((@width**2) + (@height**2))).to_f
+  end
+  
+  def square?
+      @height == @width
+  end
+  
 end
+
 
 
 
@@ -35,7 +53,14 @@ end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 
+rectangle1 = Rectangle.new(10,12)
+rectangle2 = Rectangle.new(12,12)
+rectangle3 = Rectangle.new(3,4)
 
+p rectangle1.area == 120
+p rectangle2.square? == true
+p rectangle3.diagonal == 5
+p rectangle1.perimeter == 44
 
 
 
